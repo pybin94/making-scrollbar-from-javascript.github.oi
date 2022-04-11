3f63fe638be939f8cb52390a0712dad483cee4f5
pybin94 <73331322+pybin94@users.noreply.github.com>
2022-04-11 17:29:27 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-const =  scrollBar(padding, border) => {
+const scrollBar = (padding, border) => {
 
-    const contentBox = document.querySelector("#contents")
-    const box = document.querySelector("#box")
-    const scrollBarCover = document.querySelector("#scrollBarCover")
-    const scrollBar = document.querySelector("#scrollBar")
+  const contentBox = document.querySelector("#contents")
+  const box = document.querySelector("#box")
+  const scrollBarCover = document.querySelector("#scrollBarCover")
+  const scrollBar = document.querySelector("#scrollBar")
+  
+  contentBox.addEventListener('scroll', () => {
+    // A contentbox (내용이 들어가는 박스)의 전체 높이(스크롤 포함) - 박스의 높이 + border 값 2(1+1)
+    let conBoxHeight = contentBox.scrollHeight - box.offsetHeight + padding + border  
+    // console.log(conBoxHeight)
+
+    // B 스크롤 top 적용하기 위해 스크롤 바를 감싼(scrollBarCover) 박스의 높이 - 스크롤바(scrollBar)의 높이
+    // console.log(scrollBarCover.offsetHeight-scrollBar.offsetHeight)  
+  
+    console.log(scrollBar.offsetHeight)
+    const conBoxST = conBoxHeight / ( scrollBarCover.offsetHeight - scrollBar.offsetHeight )  // A ÷ B
     
-      contentBox.addEventListener('scroll', function() {
-        // A contentbox (내용이 들어가는 박스)의 전체 높이(스크롤 포함) - 박스의 높이 + border 값 2(1+1)
-        let conBoxHeight = contentBox.scrollHeight-box.offsetHeight+padding+border  
-        // console.log(conBoxHeight)
-
-        // B 스크롤 top 적용하기 위해 스크롤 바를 감싼(scrollBarCover) 박스의 높이 - 스크롤바(scrollBar)의 높이
-        // console.log(scrollBarCover.offsetHeight-scrollBar.offsetHeight)  
-     
-        // console.log(scrollBar.offsetHeight)
-        const conBoxST = conBoxHeight/(scrollBarCover.offsetHeight-scrollBar.offsetHeight)  // A ÷ B
-        
-        // console.log(conBoxST);
-        // console.log(contentBox.scrollTop/conBoxST) // 스크롤 위치와 스크롤 바의 높이값을 비율로 계산
-        scrollBar.style.top = contentBox.scrollTop/conBoxST+"px"
-    });
+    // console.log(conBoxST);
+    // console.log(contentBox.scrollTop/conBoxST) // 스크롤 위치와 스크롤 바의 높이값을 비율로 계산
+    scrollBar.style.top = contentBox.scrollTop / conBoxST + "px"
+  });
 }
 
 // padding = 40 // 박스의 padding-top + padding-bottom 값 입력 
